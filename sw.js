2fbd69b060d59ba1a75dfb460ba549ddb1f836ef
kinglordgez-lgtm <kinglordgez@gmail.com>
2026-08-27 19:15:05 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,4 @@
+const CACHE='cosmic-v04';
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.json','./icon.svg'])))});
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(CACHE).then(cache=>cache.put(e.request,c));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))))});
