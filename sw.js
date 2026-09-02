ccfe26a29855e31bf76a5927fcc17fffc0af84e4
kinglordgez-lgtm <kinglordgez@gmail.com>
2026-09-02 21:12:07 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='cosmic-v0880';
+const CACHE='cosmic-v0890';
 self.addEventListener('install',event=>{
   self.skipWaiting();
   event.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html'])));
