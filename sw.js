46ca915b8733a2d6d34d6ff76a5b00478caef2dc
kinglordgez-lgtm <kinglordgez@gmail.com>
2026-09-02 20:36:28 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,27 @@
-self.addEventListener('fetch',()=>{});
+const CACHE='cosmic-v0862';
+self.addEventListener('install',event=>{
+  self.skipWaiting();
+  event.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html'])));
+});
+self.addEventListener('activate',event=>{
+  event.waitUntil(
+    caches.keys()
+      .then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
+      .then(()=>self.clients.claim())
+  );
+});
+self.addEventListener('fetch',event=>{
+  if(event.request.mode==='navigate'){
+    event.respondWith(
+      fetch(event.request,{cache:'no-store'})
+        .then(response=>{
+          const copy=response.clone();
+          caches.open(CACHE).then(c=>c.put('./index.html',copy));
+          return response;
+        })
+        .catch(()=>caches.match('./index.html'))
+    );
+    return;
+  }
+  event.respondWith(fetch(event.request).catch(()=>caches.match(event.request)));
+});
